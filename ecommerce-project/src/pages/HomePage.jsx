d844a9ad9b335f46722a7d7a25d2ff70d7e4cb69
Bala-Prakash-M <bala.esec2024@gmail.com>
2025-09-23 21:36:30 +0530

    Use data from the backend to generate the HTML

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,16 +1,19 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Header } from '../components/Header';
 import '../components/header.css';
-import { products } from '../../starting-code/data/products'
+//import { products } from '../../starting-code/data/products'
 import './HomePage.css';
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-    .then((response) => {
-      return response.json();
-    }).then((data) => {
-      console.log(data);
-    });
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    axios.get('http://localhost:3000/api/products')
+      .then((response) => {
+        setProducts(response.data);
+    });
+  }, []);
 
   return (
     <>
